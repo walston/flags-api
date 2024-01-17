f8aca835bc8d10a6b2c36dd6b2d038c4461afcaa
streetlightkids <ltj_hq@hotmail.com>
2024-01-17 11:25:56 -0700

    Create Header.tsx

---
+++ src/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="Header">
+      <p className="site-title">Where in the world?</p>{" "}
+      <button className="ui-toggle">☾ Dark Mode</button>
+    </header>
+  );
+}
